d9c23026673dba19579aae0f89d977816cddb812
Gidi van Kruijsdijk <gidi@k-bit.dev>
2021-03-26 23:34:19 +0100

    Fixed opening and closing create story modal

--- scripts/viewsScript.php
+++ scripts/viewsScript.php
@@ -4,12 +4,12 @@ include_once('../classes/View.php');
 if(isset($_GET['createStory'])){
 	$view = new View();
 	$story = $view->createStoryTjView();
-	echo $story;
+	return $story;
 } 
 
 if(isset($_GET['createFuck'])) {
 	$view = new View();
 	$fuck = $view->createFuckTjView();
-	echo $fuck;
+	return $fuck;
 }
 ?>
